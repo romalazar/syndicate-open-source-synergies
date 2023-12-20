3875eb58b8c800dc2852ffee8f53ebcf4e46eebf
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-12-20 20:43:02 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,13 +1,14 @@
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
 
 
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-}
-    public static void main(String[] args) {
-    }
-        for (int item : data) {
-        }
+    return data
+def main():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+    for item in data:
+
+    main()
+    data = generate_random_data()
